61e8256fc09ae46164af158f349aa326f0636d1a
yamanidev <elamine.yamani@gmail.com>
2021-04-02 23:36:34 +0100

    Full timer functionality + stop functionality

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
+// DOM elements
 let startBtn = document.getElementById('start');
 let resetBtn = document.getElementById('reset');
-let timer = document.getElementById('timer');
-let x = 25*60;
+let stopBtn = document.getElementById('stop');
+let timerText = document.getElementById('timer');
+// End DOM elements
 
-function countdown(){
-    function yo(){
-        --x;
-        timer.innerText = (~~(x/60)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}) + ":" + (x%60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+// Working time minutes*seconds
+let sessionDuration = 25*60;
+let timerInterval;
+
+// Event listeners
+startBtn.addEventListener('click', function(){
+    timerInterval = setInterval(startTimer, 10);
+})
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerInterval);
+});
+
+resetBtn.addEventListener("click", function(){
+    sessionDuration = 25*60;
+    if (timerInterval != undefined){
+        clearInterval(timerInterval);
     }
+    updateTimer();
+})
 
-    let y = setInterval(yo, 1000);
+// End event listeners
+
+function updateTimer(){
+    timerText.innerText = (~~(sessionDuration / 60)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}) + ":" + (sessionDuration % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
 }
-startBtn.onclick = countdown;
+
+function startTimer(){
+    --sessionDuration;
+    if (sessionDuration <= 0){
+        clearInterval(timerInterval);
+    }
+    updateTimer();
+}
+
+
